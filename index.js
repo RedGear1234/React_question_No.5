8bcf8171d5216b106a500bb424abde38f5097220
RedGear1234 <shubhamchavan@live.com>
2023-04-08 14:51:43 +0530

    Changes has been made

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-const fs = require('fs')
+const fs = require('fs');
+
+fs.writeFileSync("Read.txt", "this is read file");
+
+async function readFile (path) {
+try {
+    const data = await fs.readFileSync(path, {encoding : 'utf8'})
+    return data
+} catch (error) {
+    throw error
+}
+}
+
+async function main() {
+    try {
+        const data = await readFile('Read.txt')
+        console.log(data)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
+main()
